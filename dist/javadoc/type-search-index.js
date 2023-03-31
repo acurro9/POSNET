bca9612330f6cf163d11a7d3931af5290d6dd83e
acurro9 <116898672+acurro9@users.noreply.github.com>
2023-03-31 19:56:20 +0100

    Delete dist/javadoc directory

--- dist/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"posnet","l":"EntidadFinanciera"},{"p":"posnet","l":"Persona"},{"p":"posnet","l":"POSNET"},{"p":"posnet","l":"Principal"},{"p":"posnet","l":"TarjetaDeCredito"},{"p":"posnet","l":"Ticket"}];updateSearchResults();
